723fadccce2a6a8b2a4eb1989ba419c4e7f0e943
caleb-007 <calebareeveso7@gmail.com>
2020-06-23 10:25:14 +0100

    an update

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ window.onload = function getUsers(){
     });
     document.getElementById('output').innerHTML = output;
   })
+  
+
   setTimeout(function sortTables() {
     var table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById("myTable");
@@ -44,6 +46,29 @@ window.onload = function getUsers(){
       } 
     }
     document.get
+  },2000)
+  setTimeout(function() {
+    var x = document.getElementsByTagName("TR");
+   x[1].style.backgroundColor = "rgb(70, 247, 114)";
+  },2250)
+  setTimeout(function () {
+    var x = document.getElementsByTagName("TR");
+   x[2].style.backgroundColor = " rgb(70, 247, 114)";
   },2500)
+  setTimeout(function () {
+    var x = document.getElementsByTagName("TR");
+   x[3].style.backgroundColor = " rgb(70, 247, 114)";
+  },3000)
+
+  
+  setTimeout(function(){
+    var x = document.getElementById("snackbar");
+  
+    x.className = "shows";
+  
+
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+
+  },3500)
 }
 
