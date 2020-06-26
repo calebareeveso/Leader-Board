8d79f60e3d203ba4c601a865dd2a56241e14b988
caleb-007 <calebareeveso7@gmail.com>
2020-06-26 20:38:13 +0100

    an update

--- app.js
+++ app.js
@@ -27,20 +27,16 @@ window.onload = function getUsers(){
       rows = table.rows;
       for (i = 1; i < (rows.length - 1); i++) {
         shouldSwitch = false;
-        /*Get the two elements you want to compare,
-        one from current row and one from the next:*/
+        
         x = rows[i].getElementsByTagName("TD")[3];
         y = rows[i + 1].getElementsByTagName("TD")[3];
-        //check if the two rows should switch place:
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
           shouldSwitch = true;
           break;
         }
       }
       if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
+      
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
       } 
